805cf4a86e4c009e2f2bd5998f8f9c7a5edab5cd
akosijovi <jftorrevillas@gmail.com>
2018-09-02 14:09:34 +0800

    Modified BurgerBuilder to pass a prop to Burger Component that is used to display the ingredients dynamically.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,21 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => (
-    <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="bread-bottom" />
-    </div>
-);
+const burger = (props) => {
+    const dynamicIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map( (_, idx) => {
+                return <BurgerIngredient key={igKey + idx} type={igKey} />
+            })
+        })
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {dynamicIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
 
 export default burger;
