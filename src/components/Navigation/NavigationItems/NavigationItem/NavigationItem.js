fd37717636fe5dc1c66852a82f3e1f82d0837be3
akosijovi <jftorrevillas@gmail.com>
2018-09-03 00:13:59 +0800

    Code Refactoring: Added validation for NavigationItem Props, and modified Modal to only update when needed.

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './NavigationItem.css';
 
-const navigationitem = (props) => {
-    return (
-        <li className={classes.NavigationItem}>
-            <a 
-                href={props.link}
-                className={props.active ? classes.active : null}>{props.children}</a>
-        </li>
-    )
+class NavigationItem extends Component {
+    render () {
+        return (
+            <li className={classes.NavigationItem}>
+                <a 
+                    href={this.props.link}
+                    className={this.props.active ? classes.active : null}>{this.props.children}</a>
+            </li>
+        )
+    }
+}
+NavigationItem.propTypes = {
+    link: PropTypes.string,
+    active: PropTypes.bool,
 }
 
-export default navigationitem;
+export default NavigationItem;
