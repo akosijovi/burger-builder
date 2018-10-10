fd7c84c3861b69f1a4328d95c6c3735792f164c1
akosijovi <jftorrevillas@gmail.com>
2018-10-10 21:53:47 +0800

    Retrieves data from the database and displays it to the orders page.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import axiosErrorHandler from '../../hoc/axiosErrorHandler/axiosErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then( response => {
+            const fetchedOrder = [];
+            for (let key in response.data ){
+                fetchedOrder.push({
+                    ...response.data[key],
+                    id: key
+                });
+            }
+            console.log(fetchedOrder)
+            this.setState({loading: false, orders: fetchedOrder});
+        })
+        .catch( err => {
+            this.setState({loading: false});
+        });
+    }
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id} 
+                        ingredients={order.ingredients}
+                        price={order.totalPrice}
+                    />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default axiosErrorHandler(Orders, axios);
