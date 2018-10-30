0eccbe59ad2f56fa7faa8203dfcddd50cc166520
akosijovi <jftorrevillas@gmail.com>
2018-10-31 00:18:52 +0800

    Signup: getting token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -13,14 +14,33 @@ export const authSuccess = (authData) => {
     }
 }
 
-export const authFail = () => {
+export const authFail = (error) => {
     return {
-        type: actionTypes.AUTH_FAIL
+        type: actionTypes.AUTH_FAIL,
+        error: error
     }
 }
 
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart())
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBinBwBCgC2_1s2V0Z5kJTQ6PTGRCrGJss', authData)
+            .then(
+                response => {
+                    console.log(response);
+                    dispatch(authSuccess(response.data));
+                }
+            )
+            .catch(
+                error => {
+                    console.log(error);
+                    dispatch(authFail(error));
+                }
+            )
     }
 }
